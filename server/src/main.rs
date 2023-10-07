b795169c06650cdee1953879b26f515d1e9b4aa6
Igor von Nyssen <igor@vonnyssen.com>
2023-10-07 16:36:58 -0700

    first connection to simulator

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,79 +1,90 @@
-use serde::{Deserialize, Serialize};
-use surrealdb::engine::remote::ws::Ws;
-use surrealdb::opt::auth::Root;
-use surrealdb::sql::Thing;
-use surrealdb::Surreal;
+use ascom_alpaca::api::{Camera, ServerInfo, TypedDevice};
+use ascom_alpaca::Client;
+use axum::extract::State;
+use axum::{routing::get, Router};
+use std::collections::HashSet;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tracing::error;
 
-#[derive(Debug, Serialize)]
-struct Name<'a> {
-    first: &'a str,
-    last: &'a str,
-}
-
-#[derive(Debug, Serialize)]
-struct Person<'a> {
-    title: &'a str,
-    name: Name<'a>,
-    marketing: bool,
-}
-
-#[derive(Debug, Serialize)]
-struct Responsibility {
-    marketing: bool,
-}
-
-#[derive(Debug, Deserialize)]
-struct Record {
-    #[allow(dead_code)]
-    id: Thing,
+#[derive(Debug)]
+struct AppState {
+    server_url: String,
+    server_info: ServerInfo,
+    client: Client,
+    cameras: HashSet<Arc<dyn Camera>>,
 }
 
 #[tokio::main]
-async fn main() -> surrealdb::Result<()> {
-    // Connect to the server
-    let db = Surreal::new::<Ws>("127.0.0.1:3030").await?;
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
 
-    // Signin as a namespace, database, or root user
-    db.signin(Root {
-        username: "root",
-        password: "root",
-    })
-    .await?;
+    // create a client
+    let client = match Client::new("http://localhost:32323") {
+        Ok(client) => client,
+        Err(e) => {
+            error!("Error: {}", e);
+            return;
+        }
+    };
 
-    // Select a specific namespace / database
-    db.use_ns("test").use_db("test").await?;
+    // `get_server_info` returns high-level metadata of the server.
+    let server_info = match client.get_server_info().await {
+        Ok(info) => info,
+        Err(e) => {
+            error!("Error: {}", e);
+            return;
+        }
+    };
 
-    // Create a new person with a random id
-    let created: Vec<Record> = db
-        .create("person")
-        .content(Person {
-            title: "Founder & CEO",
-            name: Name {
-                first: "Tobie",
-                last: "Morgan Hitchcock",
-            },
-            marketing: true,
-        })
-        .await?;
-    dbg!(created);
+    let cameras = match client.get_devices().await {
+        Ok(devices) => devices
+            .filter_map(|device| match device {
+                TypedDevice::Camera(camera) => Some(camera),
+                _ => None,
+            })
+            .collect::<HashSet<_>>(),
+        Err(e) => {
+            error!("Error: {}", e);
+            return;
+        }
+    };
 
-    // Update a person record with a specific id
-    let updated: Option<Record> = db
-        .update(("person", "jaime"))
-        .merge(Responsibility { marketing: true })
-        .await?;
-    dbg!(updated);
+    let state = AppState {
+        server_url: String::from("http://localhost:32323"),
+        server_info,
+        client,
+        cameras,
+    };
 
-    // Select all people records
-    let people: Vec<Record> = db.select("person").await?;
-    dbg!(people);
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/camera", get(get_camera))
+        .with_state(Arc::new(state));
 
-    // Perform a custom advanced query
-    let groups = db
-        .query("SELECT marketing, count() FROM type::table($table) GROUP BY marketing")
-        .bind(("table", "person"))
-        .await?;
-    dbg!(groups);
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
 
-    Ok(())
+// basic handler that responds with a static string
+async fn root(State(state): State<Arc<AppState>>) -> String {
+    format!("{:?}", state)
+}
+
+async fn get_camera(State(state): State<Arc<AppState>>) -> String {
+    let camera = match state.cameras.iter().next() {
+        Some(camera) => camera,
+        None => return String::from("No camera found"),
+    };
+    format!("{:?}", camera)
 }
